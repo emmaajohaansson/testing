387add9819f3b7d1fab61d2cb9d7397ea74693f5
Emma <emmakristina95@hotmail.com>
2018-04-13 15:09:53 +0200

    Testing push of changes

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
+/*This is the index.php-file of lab 1 and 2*/
 <?php
-require 'vendor/autoload.php';
+  require 'vendor/autoload.php';
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+  use Monolog\Logger;
+  use Monolog\Handler\StreamHandler;
 
-$log = new Logger('Laboration 1');
-$log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
+  $log = new Logger('Laboration 1');
+  $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
 
   //echo "Hello World!";
 
-  var_dump($argv);
+  /*var_dump($argv);
 
   $name = $argv[1];
-  $log->info("Hello " . $name . "! What a wonderful name!");
+  $log->info("Hello " . $name . "! What a wonderful name!");*/
+
+  $name = $_GET['name'];
+  $log->info($name);
 ?>
+<!DOCTYPE html>
+  <html>
+    <head>
+    <title>Testing, testing</title>
+    </head>
+    <body>
+      <?php
+        echo "Hello, " . $name;
+      ?>
+    </body>
+  </html>
